8ea4fcec35d93454bf5a963f9720e19bb470d993
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2021-06-27 14:41:35 +0100

    Revert

---
+++ binary/client/Linux-Build/current_version.h
@@ -0,0 +1,2 @@
+#pragma once
+#define CURRENT_VERSION 1624127653ull
---
+++ binary/client/Linux-Build/external/discord-rpc/src/CMakeFiles/discord-rpc.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/repos/C-DRIVE/Aura-CL/external/discord-rpc/src/connection_unix.cpp" "/mnt/d/repos/C-DRIVE/Aura-CL/Linux-Build/external/discord-rpc/src/CMakeFiles/discord-rpc.dir/connection_unix.cpp.o"
+  "/mnt/d/repos/C-DRIVE/Aura-CL/external/discord-rpc/src/discord_register_linux.cpp" "/mnt/d/repos/C-DRIVE/Aura-CL/Linux-Build/external/discord-rpc/src/CMakeFiles/discord-rpc.dir/discord_register_linux.cpp.o"
+  "/mnt/d/repos/C-DRIVE/Aura-CL/external/discord-rpc/src/discord_rpc.cpp" "/mnt/d/repos/C-DRIVE/Aura-CL/Linux-Build/external/discord-rpc/src/CMakeFiles/discord-rpc.dir/discord_rpc.cpp.o"
+  "/mnt/d/repos/C-DRIVE/Aura-CL/external/discord-rpc/src/rpc_connection.cpp" "/mnt/d/repos/C-DRIVE/Aura-CL/Linux-Build/external/discord-rpc/src/CMakeFiles/discord-rpc.dir/rpc_connection.cpp.o"
+  "/mnt/d/repos/C-DRIVE/Aura-CL/external/discord-rpc/src/serialization.cpp" "/mnt/d/repos/C-DRIVE/Aura-CL/Linux-Build/external/discord-rpc/src/CMakeFiles/discord-rpc.dir/serialization.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISCORD_LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/discord-rpc/include"
+  "../external/discord-rpc/thirdparty/rapidjson-1.1.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
