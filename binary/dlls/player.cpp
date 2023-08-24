de670938c2807fbba3d8488bfedffbdab4867ff3
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2023-08-24 11:19:39 +0100

    Use better sound for SER

--- binary/dlls/player.cpp
+++ binary/dlls/player.cpp
@@ -4665,20 +4665,19 @@ void CBasePlayer :: UpdateClientData( void )
 		{
 			m_flNextSuitRegenTime = 0.0f;
 			m_fRegenOn = false;
-			STOP_SOUND(ENT(pev), CHAN_BODY, "player/shield_charge.wav");
-			//EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/suitchargeno1.wav", 0.85, ATTN_NORM);
+			STOP_SOUND(ENT(pev), CHAN_STATIC, "items/suitcharge1.wav");
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/suitchargeno1.wav", 0.85, ATTN_NORM);
 		}
 		else if (!m_fRegenOn) // when shield starts recharging
 		{
 			m_fRegenOn = true;
-			//EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/suitchargeok1.wav", 0.85, ATTN_NORM);
-			EMIT_SOUND(ENT(pev), CHAN_BODY, "player/shield_charge.wav", 0.85, ATTN_NORM);
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/suitchargeok1.wav", 0.85, ATTN_NORM);
 		}
 		else // as it's recharging
 		{
-			STOP_SOUND(ENT(pev), CHAN_STATIC, "player/shield_charge.wav");
+			STOP_SOUND(ENT(pev), CHAN_STATIC, "items/suitcharge1.wav");
 			// too loud
-			//EMIT_SOUND(ENT(pev), CHAN_STATIC, "player/shield_charge.wav", 0.25, ATTN_NORM);
+			EMIT_SOUND(ENT(pev), CHAN_STATIC, "items/suitcharge1.wav", 0.25, ATTN_NORM);
 		}
 
 		m_flNextSuitRegenTime = gpGlobals->time + sv_aura_regeneration_wait.value;
