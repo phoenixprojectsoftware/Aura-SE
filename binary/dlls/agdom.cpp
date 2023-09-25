c391d0363e49e4268ee175c689ede2d42ce77b31
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2023-09-25 02:25:37 +0100

    Avoid character limit

--- binary/dlls/agdom.cpp
+++ binary/dlls/agdom.cpp
@@ -266,7 +266,7 @@ void AgDOMControlPoint::Capture(CBasePlayer* pPlayer, const char* szTeamName)
     // Inform all players that zone has been taken control of by playername	
     // really need identifiers for each control point! 
     char szText[300];
-    sprintf(szText, "%s captures control point at %s!", STRING(pPlayer->pev->netname), m_szLocation);
+    sprintf(szText, "%s captured %s!", STRING(pPlayer->pev->netname), m_szLocation);
     AgConsole(szText);
     UTIL_ClientPrintAll(HUD_PRINTCENTER, szText);
 }
@@ -381,9 +381,9 @@ void AgDOMControlPoint::Think(void)
     // a neutral state. 
     if (m_iConsecutiveScores >= ag_dom_resetscorelimit.value) {
         char szText[201];
-        sprintf(szText, "Neutral control point available at %s", m_szLocation);
+        sprintf(szText, "Control point available at %s", m_szLocation);
         AgConsole(szText);
-        UTIL_ClientPrintAll(HUD_PRINTCENTER, szText);
+        UTIL_ClientPrintAll(HUD_PRINTTALK, szText);
         Reset();
     }
 
