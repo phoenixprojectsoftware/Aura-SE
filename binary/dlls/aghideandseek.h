dd1d48d69183fb6ea028cbc5f2370057b36b6a9b
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2023-09-25 02:48:35 +0100

    Try to fix makefile

--- binary/dlls/aghideandseek.h
+++ binary/dlls/aghideandseek.h
@@ -4,10 +4,14 @@
 
 //#if !defined(AFX_AGARENA_H__1929C55A_3034_4C89_8398_1F8243B83499__INCLUDED_)
 #define AFX_AGARENA_H__1929C55A_3034_4C89_8398_1F8243B83499__INCLUDED_
+#ifndef HIDEANDSEEK_H
+#define HIDEANDSEEK_H
 
 #if _MSC_VER > 1000
 #pragma once
 #endif // _MSC_VER > 1000
+
+
 
 class AgHideandseek
 {
@@ -44,8 +48,8 @@ public:
     void ClientDisconnected(CBasePlayer* pPlayer);
     void ClientConnected(CBasePlayer* pPlayer);
 
-   // bool CanTakeDamage();
-    //bool CanHaveItem();
+    bool CanTakeDamage();
+    bool CanHaveItem();
 
 };
 
@@ -66,7 +70,8 @@ inline CBasePlayer* AgHideandseek::GetSeeker()
         return NULL;
 };
 
-/*
+
+
 inline bool AgHideandseek::CanTakeDamage()
 {
     return Playing == m_Status || PlayerDied == m_Status;
@@ -76,7 +81,7 @@ inline bool AgHideandseek::CanHaveItem()
 {
     return Spawning == m_Status;
 }
-*/
 
 
 //#endif // !defined(AFX_AGARENA_H__1929C55A_3034_4C89_8398_1F8243B83499__INCLUDED_)
+#endif // HIDEANDSEEK_H
