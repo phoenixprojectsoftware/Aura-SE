3dddca2727f84ed9757fe5cb3928e2722d242928
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-09-02 23:56:37 +0100

    Fix ;

--- binary/dlls/player.cpp
+++ binary/dlls/player.cpp
@@ -467,7 +467,7 @@ void CBasePlayer :: DeathSound( void )
 	}
 
 	// play one of the suit death alarms
-	if (INSTAGIB != AgGametype()) { EMIT_GROUPNAME_SUIT(ENT(pev), "HEV_DEAD") };
+	if (INSTAGIB != AgGametype()) { EMIT_GROUPNAME_SUIT(ENT(pev), "HEV_DEAD"); };
 }
 
 // override takehealth
