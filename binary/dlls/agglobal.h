yhoiluhi/agmod

Add satchel destructibility and solidity settings
New cvars:
* sv_ag_satchel_destroyable <0|1> - Default: 0 (indestructible)
* sv_ag_satchel_health <number> - Default: 15
* sv_ag_satchel_solid <0|1> - Default: 1 (solid)

Satchels only be destroyed with blast/radius damage.
I have tried a few things (added a comment/fixme) but couldn't
make them killable with hitscan or other damage types, so
at the moment you cannot destroy them with e.g. a glock.
Any help with that is very appreciated :))

dlls/agglobal.h
@@ -187,6 +187,10 @@ extern cvar_t ag_fps_limit_punishment_slap_intensity;
 extern cvar_t ag_fps_limit_punishment_slap_interval;
 extern cvar_t ag_fps_limit_punishment_ban_time;
 
+extern cvar_t ag_satchel_destroyable;
+extern cvar_t ag_satchel_health;
+extern cvar_t ag_satchel_solid;
+
 extern bool g_bLangame;
 extern bool g_bUseTeamColors;
 

phoenixprojectsoftware/Aura-SE

binary/dlls/agglobal.h
@@ -202,6 +202,18 @@ extern cvar_t ag_spawn_pa_visible_chance;
 extern cvar_t ag_spawn_pa_audible_chance;
 extern cvar_t ag_spawn_pa_safe_chance;
 
+extern cvar_t ag_fps_limit;
+extern cvar_t ag_fps_limit_auto;
+extern cvar_t ag_fps_limit_check_interval;
+extern cvar_t ag_fps_limit_match_only;
+extern cvar_t ag_fps_limit_steampipe;
+extern cvar_t ag_fps_limit_warnings;
+extern cvar_t ag_fps_limit_warnings_interval;
+extern cvar_t ag_fps_limit_punishment;
+extern cvar_t ag_fps_limit_punishment_slap_intensity;
+extern cvar_t ag_fps_limit_punishment_slap_interval;
+extern cvar_t ag_fps_limit_punishment_ban_time;
+
 extern bool g_bLangame;
 extern bool g_bUseTeamColors;
 