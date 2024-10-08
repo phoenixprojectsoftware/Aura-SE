23a3b0a8ebf5fd65feeded042a303a868c392953
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-10-09 00:57:17 +0100

    Remove game_player_equip and player_weaponstrip
    This was causing so many problems with certain maps & gamemodes so it's better to remove the entities from the code entirely.

--- binary/dlls/player.cpp
+++ binary/dlls/player.cpp
@@ -5690,35 +5690,6 @@ void CDeadHEV :: Spawn( void )
 	MonsterInitDead();
 }
 
-
-class CStripWeapons : public CPointEntity
-{
-public:
-	void	Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
-
-private:
-};
-
-LINK_ENTITY_TO_CLASS( player_weaponstrip, CStripWeapons );
-
-void CStripWeapons :: Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
-{
-	CBasePlayer *pPlayer = NULL;
-
-	if ( pActivator && pActivator->IsPlayer() )
-	{
-		pPlayer = (CBasePlayer *)pActivator;
-	}
-	else if ( !g_pGameRules->IsDeathmatch() )
-	{
-		pPlayer = (CBasePlayer *)CBaseEntity::Instance( g_engfuncs.pfnPEntityOfEntIndex( 1 ) );
-	}
-
-	if ( pPlayer )
-		pPlayer->RemoveAllItems( FALSE );
-}
-
-
 class CRevertSaved : public CPointEntity
 {
 public:
