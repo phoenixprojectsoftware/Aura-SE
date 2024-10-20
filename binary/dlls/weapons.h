3494c9bb031b88078bf13e732e14f411d5d8a33e
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-10-20 20:26:06 +0100

    .

--- binary/dlls/weapons.h
+++ binary/dlls/weapons.h
@@ -152,7 +152,7 @@ public:
 #define WEAPON_NOCLIP			-1
 
 //#define CROWBAR_MAX_CLIP		WEAPON_NOCLIP
-#define GLOCK_MAX_CLIP			17
+#define GLOCK_MAX_CLIP			18
 #define ONE_MAX_CLIP			WEAPON_NOCLIP
 #define PYTHON_MAX_CLIP			6
 #define MP5_MAX_CLIP			60
