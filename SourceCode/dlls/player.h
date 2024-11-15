20932137a0247bca9916f2f319b909e263f5359a
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-11-15 01:02:57 +0000

    Observer mode comments

--- SourceCode/dlls/player.h
+++ SourceCode/dlls/player.h
@@ -722,12 +722,12 @@ inline bool CBasePlayer::IsTeammate(CBaseEntity* pTarget)
 }
 
 // Spectator Movement modes (stored in pev->iuser1, so the physics code can get at them)
-#define OBS_NONE				0
-#define OBS_CHASE_LOCKED		1
-#define OBS_CHASE_FREE			2
-#define OBS_ROAMING				3		
-#define OBS_IN_EYE				4
-#define OBS_MAP_FREE			5
+#define OBS_NONE				0 // not observing.
+#define OBS_CHASE_LOCKED		1 // Chase Locked
+#define OBS_CHASE_FREE			2 // Chase Free-Cam
+#define OBS_ROAMING				3 // Roaming
+#define OBS_IN_EYE				4 // First-Person
+#define OBS_MAP_FREE			5 // Map Free Roam
 #define OBS_MAP_CHASE			6
 
 //-- Martin Webrant
