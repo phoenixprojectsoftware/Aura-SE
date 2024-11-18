888e26a7bb0622c30ac0408ec8ee6f5d5deeb1d7
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-11-18 20:27:44 +0000

    Fix function stopping the wrong sound

--- SourceCode/dlls/player.cpp
+++ SourceCode/dlls/player.cpp
@@ -785,7 +785,7 @@ int CBasePlayer :: TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, 
 	// BlueNightHawk : Suit Energy Regeneration || after damage taken!!
 	if (sv_aura_regeneration.value != 0 && pev->armorvalue < MAX_NORMAL_BATTERY && fTookDamage)
 	{
-		STOP_SOUND(ENT(pev), CHAN_STATIC, "items/suitcharge1.wav");
+		STOP_SOUND(ENT(pev), CHAN_STATIC, "items/suitcharge_no_lp.wav");
 		if (m_fRegenOn == true)
 			EMIT_SOUND(ENT(pev), CHAN_ITEM, "player/shield_disrupted.wav", 0.85, ATTN_NORM); // low-pitched elevbell
 		m_flNextSuitRegenTime = gpGlobals->time + 5.5 + sv_aura_regeneration_wait.value;
