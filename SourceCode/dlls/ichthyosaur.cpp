f735f221f817d0f7ccca1ce69dfe6d8763572e80
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-08-23 16:39:25 +0100

    Remove coop mode spawn restriction from monsters
    Eliminated the check for 'sv_aura_coop' in the Spawn() methods of various monster and NPC classes, allowing them to spawn regardless of coop mode. Also updated the MODNAME in the Linux build script from 'aura_linux' to 'aura'.

--- SourceCode/dlls/ichthyosaur.cpp
+++ SourceCode/dlls/ichthyosaur.cpp
@@ -476,16 +476,6 @@ void CIchthyosaur :: HandleAnimEvent( MonsterEvent_t *pEvent )
 //=========================================================
 void CIchthyosaur :: Spawn()
 {
-	// Only spawn if coopmode is enabled
-	if (CVAR_GET_FLOAT("sv_aura_coop") == 1)
-	{
-		CBaseMonster::Spawn(); // Proceed with spawning if coopmode is on
-	}
-	else
-	{
-		// Optionally, log or remove entity if coopmode is off
-		UTIL_Remove(this);
-	}
 	Precache( );
 
 	SET_MODEL(ENT(pev), "models/icky.mdl");
--- SourceCode/dlls/zamn.cpp
+++ SourceCode/dlls/zamn.cpp
@@ -262,16 +262,6 @@ void CZamnZombie::HandleAnimEvent(MonsterEvent_t* pEvent)
 //=========================================================
 void CZamnZombie::Spawn()
 {
-	// Only spawn if coopmode is enabled
-	if (CVAR_GET_FLOAT("sv_aura_coop") == 1)
-	{
-		CBaseMonster::Spawn(); // Proceed with spawning if coopmode is on
-	}
-	else
-	{
-		// Optionally, log or remove entity if coopmode is off
-		UTIL_Remove(this);
-	}
 	Precache();
 
 	SET_MODEL(ENT(pev), "models/zamnzombie.mdl");
