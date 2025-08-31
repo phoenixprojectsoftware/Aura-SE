5414b5774177abfac78a787d527cc905ae87b24f
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-08-31 21:44:37 +0100

    Update hgrunt.cpp

--- SourceCode/dlls/hgrunt.cpp
+++ SourceCode/dlls/hgrunt.cpp
@@ -1007,7 +1007,7 @@ void CHGrunt :: Spawn()
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_RED;
 	pev->effects		= 0;
-	pev->health			= gSkillData.hgruntHealth;
+	pev->health			= gSkillData.hgruntHealth * 4;
 	m_flFieldOfView		= 0.2;// indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState		= MONSTERSTATE_NONE;
 	m_flNextGrenadeCheck = gpGlobals->time + 1;
