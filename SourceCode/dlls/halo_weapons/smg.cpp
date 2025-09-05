682d7bd0950fabf802c20016413ea000e98ed03b
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-09-05 17:13:50 +0100

    Fix Excession SMG clientside events

--- SourceCode/dlls/halo_weapons/smg.cpp
+++ SourceCode/dlls/halo_weapons/smg.cpp
@@ -132,7 +132,7 @@ void CSMG::PrimaryAttack()
 	flags = 0;
 #endif
 
-	PLAYBACK_EVENT_FULL(flags, m_pPlayer->edict(), m_usM7, 0.0, (float*)&g_vecZero, (float*)&g_vecZero, vecDir.x, vecDir.y, 0, 0, 0, 0);
+	PLAYBACK_EVENT_FULL(0, m_pPlayer->edict(), m_usM7, 0.0, (float*)&g_vecZero, (float*)&g_vecZero, vecDir.x, vecDir.y, 0, 0, 0, 0);
 
 	if (0 == m_iClip && m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] <= 0)
 		// HEV suit - indicate out of ammo condition
