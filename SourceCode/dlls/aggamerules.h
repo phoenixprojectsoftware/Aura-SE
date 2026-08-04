5d5331894d6a4cb04a0876fa8a8499c9c39741bb
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2026-08-04 01:48:58 +0100

    Revert "Part 4 - map validation"
    This reverts commit a7881dc143898d97e08e7ba69fc18835dfac801a.

--- SourceCode/dlls/aggamerules.h
+++ SourceCode/dlls/aggamerules.h
@@ -48,8 +48,6 @@ protected:
     float m_flInvalidMapChangeTime;
     int m_iLastInvalidMapCountdown;
 
-    bool m_bNativeSpawnValidationDone;
-
 public:
     AgGameRules();
     virtual ~AgGameRules();
@@ -107,12 +105,6 @@ public:
     void SendMapListToClient(CBasePlayer* pPlayer, bool bStart);
 
     void TestInvalidSequence();
-
-    bool UsesNativeDeathmatchSpawns() const;
-    int CountNativeDeathmatchSpawns() const;
-
-    AgMapValidationResult ValidateNativeDeathmatchSpawns() const;
-    void ValidateNativeMapOnce();
 
     const char* GetIPAddress(edict_t* pEntity);
     bool    m_bProxyConnected;
